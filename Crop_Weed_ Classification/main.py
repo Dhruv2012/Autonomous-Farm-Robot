ab24bec56cfef3bb7c80937aca0f9281cdf3e0ed
Dhruv Patel <30570613+Dhruv2012@users.noreply.github.com>
2019-12-11 12:16:43 +0530

    Pre-prelims results done...
    Cropvsweedv1.h5 => crop weed seg
    v1.h5 => vegetation seg

--- Crop_Weed_ Classification/main.py
+++ Crop_Weed_ Classification/main.py
@@ -18,8 +18,7 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from skimage.util import invert
 
-multiclass =  False
-retrain = True
+retrain = False
 
 
 def dice_coef(y_true, y_pred):
@@ -40,13 +39,19 @@ if(retrain == False):
         l = layers.Reshape((128*128,3))(model.output)
         output = layers.Activation("softmax")(l)
         unet_model = Model(inputs = model.input, outputs = output)
-        unet_model.compile(loss = "categorical_crossentropy",optimizer = "Adam") 
+        unet_model.compile(loss = "categorical_crossentropy",optimizer = "Adam",  metrics=['accuracy']) 
     elif(path_y == "/home/dhruv/Final_Year_Project/Datasets/cwfid dataset(Annotated)/WithAnnotations/masks"):
         unet_model = small_Unet(1,128,128,"sigmoid")
-        unet_model.compile(loss = dice_coef_loss,optimizer = "Adam")
+        unet_model.compile(loss = dice_coef_loss,optimizer = "Adam", metrics=['accuracy'])
 else:
     print("reloading previous weights............")
-    unet_model = load_model("/home/dhruv/Final_Year_Project/Crop_Weed_ Classification/cropvsweedv1.h5")
+    if (path_y == "/home/dhruv/Final_Year_Project/Datasets/cwfid dataset(Annotated)/WithAnnotations/annotations"):
+        unet_model = load_model("/home/dhruv/Final_Year_Project/Crop_Weed_ Classification/cropvsweedv1.h5")
+        print("crop vs weeds model loaded...")
+    elif(path_y == "/home/dhruv/Final_Year_Project/Datasets/cwfid dataset(Annotated)/WithAnnotations/masks"):
+        unet_model = load_model("/home/dhruv/Final_Year_Project/Crop_Weed_ Classification/v1.h5")
+        print("vegetation seg model loaded(version 1).....")
+
 unet_model.summary()
 ###############################################
 
@@ -160,13 +165,17 @@ def load_cwfid_withyaml(path_x,path_y,path_yaml):
             crop = img[:,:,1]
             temp = np.zeros((128,128,3))
             temp[:,:, 0] = weed
-            temp[:, :, 1] = crop    
+            temp[:, :, 1] = crop
+            back = img.copy()
+            back = np.all(img == [0, 0, 0],axis=-1)*1
+            temp[:,:, 2] = back
             img = np.reshape(temp,(128*128,3))
         else:    
             y_path = os.path.join(path_y,str(i)) + "_mask.png" 
             img = load_img(y_path,target_size=(128,128),color_mode="grayscale")
-            img = invert(img)
             img = img_to_array(img,dtype="uint8")/255.
+            img = invert(img)
+            
             
         y_train.append(img)
         
@@ -184,13 +193,17 @@ def load_cwfid_withyaml(path_x,path_y,path_yaml):
             crop = img[:,:,1]
             temp = np.zeros((128,128,3))
             temp[:,:, 0] = weed
-            temp[:, :, 1] = crop    
+            temp[:, :, 1] = crop
+            back = img.copy()
+            back = np.all(img == [0, 0, 0],axis=-1)*1
+            temp[:,:, 2] = back
             img = np.reshape(temp,(128*128,3))
         else:    
             y_path = os.path.join(path_y,str(i)) + "_mask.png" 
             img = load_img(y_path,target_size=(128,128),color_mode="grayscale")
-            img = invert(img)
             img = img_to_array(img,dtype="uint8")/255.
+            img = invert(img)
+            
             
         y_test.append(img)
     x_train = np.array(x_train)
@@ -225,21 +238,48 @@ print(x_test.shape)
 print(y_train.shape)
 print(y_test.shape)
 
-history = unet_model.fit(x_train, y_train, batch_size=4, epochs=100,verbose=1,validation_split = 0.2,shuffle = True)
+history = unet_model.fit(x_train, y_train, batch_size=4, epochs= 150,verbose=1,validation_data = (x_test,y_test),shuffle = True)
 unet_model.save("/home/dhruv/Final_Year_Project/Crop_Weed_ Classification/cropvsweedv1.h5")
 plot_model(unet_model, to_file='/home/dhruv/Final_Year_Project/Crop_Weed_ Classification/unet_model_plot.png', show_shapes=True, show_layer_names=True)
 
 print("checking...")
 
-plt.subplot(1,1,1)
+plt.figure(figsize=(10,3))
+plt.subplot(1,2,1)
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('Model loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
+
+plt.subplot(1,2,2)
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
+'''
+plt.figure(figsize=(10,3))
+plt.subplot(1,3,1)
+plt.title("input")
+plt.imshow(x_test[2])
+plt.subplot(1,3,2)
+plt.title("correct label")
+plt.imshow(y_test[2,:,:,0])
+
+
+prediction = unet_model.predict(x_test[2:3])
+#prediction shape will be (1,128,128,1)
+plt.subplot(1,3,3)
+plt.title("prediction")
+plt.imshow(prediction[0,:,:,0])
+print("hey")
+plt.show()
+'''
 plt.figure(figsize=(10,3))
 plt.subplot(1,3,1)
 plt.title("input")
